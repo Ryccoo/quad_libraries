1cbf58674baa01971b3c33fca370d04efa81eb85
Richard Ludvigh <richard.ludvigh@gmail.com>
2013-11-27 14:54:04 +0100

    AccelGyro: Library for communicating with MPU6050

--- AccelGyro/AccelGyro.h
+++ AccelGyro/AccelGyro.h
@@ -2,6 +2,7 @@
 #define _AccelGyro_h
 
 #include "Energia.h"
+#include "../Wire/Wire.h"
 
 
 // The name of the sensor is "MPU-6050".
@@ -649,15 +650,33 @@ typedef struct accel_t_gyro_global
   unsigned long last_read_time;
   float         last_x_angle;  // These are the filtered angles
   float         last_y_angle;
-  float         last_z_angle;  
+  float         last_z_angle;
   float         last_gyro_x_angle;  // Store the gyro angles to compare drift
   float         last_gyro_y_angle;
   float         last_gyro_z_angle;
 };
 
+
 class AccelGyro {
 public:
-  AccelGyro() {}
+  AccelGyro();
+  void Calibrate_sensors();
+  int MPU6050_read(int start, uint8_t *buffer, int size);
+  int MPU6050_write(int start, const uint8_t *pData, int size);
+  int MPU6050_write_reg(int reg, uint8_t data);
+  int read_gyro_accel_vals(uint8_t* accel_t_gyro_ptr);
+  void set_last_read_angle_data(unsigned long time, float x, float y, float z, float x_gyro, float y_gyro, float z_gyro);
+  unsigned long get_last_time();
+  float get_last_x_angle();
+  float get_last_y_angle();
+  float get_last_z_angle();
+  float get_last_gyro_x_angle();
+  float get_last_gyro_y_angle();
+  float get_last_gyro_z_angle();
 };
+
+extern accel_t_gyro_global accel_t_gyro_global;
+extern accel_t_gyro_union accel_t_gyro;
+extern AccelGyro Gyro;
 
 #endif
